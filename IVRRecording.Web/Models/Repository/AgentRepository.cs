0cfae68f97b5fd1c17f72854bb6649528491d1c1
Agustin Camino <agustin.camino@gmail.com>
2015-10-06 09:05:29 -0500

    Add AgentRepository

---
+++ IVRRecording.Web/Models/Repository/AgentRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace IVRRecording.Web.Models.Repository
+{
+    public interface IAgentRepository
+    {
+        Agent FindByExtension(string extension);
+    }
+    public class AgentRepository : IAgentRepository
+    {
+        private readonly IVRRecordingContext _context;
+
+        public AgentRepository()
+        {
+            _context = new IVRRecordingContext();
+        }
+
+        public Agent FindByExtension(string extension)
+        {
+            return _context.Agents.FirstOrDefault(agent => agent.Extension == extension);
+        }
+    }
+}
