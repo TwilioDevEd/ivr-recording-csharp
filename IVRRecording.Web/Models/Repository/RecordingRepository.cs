fa4a7dedb0a34ff85718c2ef5956d7e6eddbfcb9
Agustin Camino <agustin.camino@gmail.com>
2015-10-06 13:38:50 -0500

    Add Recording repository

---
+++ IVRRecording.Web/Models/Repository/RecordingRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace IVRRecording.Web.Models.Repository
+{
+    public interface IRecordingRepository
+    {
+        IEnumerable<Recording> All();
+        void Create(Recording recording);
+    }
+
+    public class RecordingRepository : IRecordingRepository
+    {
+        private readonly IVRRecordingContext _context;
+
+        public RecordingRepository()
+        {
+            _context = new IVRRecordingContext();
+        }
+
+        public IEnumerable<Recording> All()
+        {
+            return _context.Recordings.ToList();
+        }
+
+        public void Create(Recording recording)
+        {
+            _context.Recordings.Add(recording);
+            _context.SaveChanges();
+        }
+    }
+}
